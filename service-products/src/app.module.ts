9f3b5b3b6ed35bf8c2a883f21e092a6fd0158792
Fernando Araujo <fernando.araujo@bilheterialdigital.com>
2024-01-17 17:43:22 -0300

    feat: Add user feature module

--- service-products/src/app.module.ts
+++ service-products/src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{ limit: 10, ttl: 60 }]),
+    MongooseModule.forRoot(process.env.DATABASE_URI, {
+      dbName: process.env.DATABASE_NAME,
+      auth: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+      },
+    }),
+    UserModule,
+  ],
+  controllers: [],
 })
 export class AppModule {}
--- service-products/src/main.ts
+++ service-products/src/main.ts
@@ -1,9 +1,42 @@
-import { NestFactory } from '@nestjs/core';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   const config = new DocumentBuilder()
+//     .setTitle('Produtos')
+//     .setDescription('API para gerenciamento de produtos')
+//     .setVersion('1.0')
+//     .addTag('products')
+//     .build();
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api', app, document);
+
+//   console.log('*** ENV', process.env.APP_PORT);
+//   await app.listen(process.env.APP_PORT || '3001');
+// }
+// bootstrap();
+
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
+import * as compression from 'compression';
+import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const PORT = parseInt(process.env.PORT, 10) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // register all plugins and extension
+  app.enableCors({ origin: '*' });
+  app.useGlobalPipes(new ValidationPipe({}));
+  app.enableVersioning({ type: VersioningType.URI });
+  app.use(helmet());
+  app.use(compression());
+
   const config = new DocumentBuilder()
     .setTitle('Produtos')
     .setDescription('API para gerenciamento de produtos')
@@ -12,7 +45,10 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  
-  await app.listen(3000);
+
+  await app.listen(PORT, () => {
+    console.log(`🚀 Application running at port ${PORT}`);
+  });
 }
+
 bootstrap();
